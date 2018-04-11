c8c2e8e25b89ab823748df5c3073aba9eb36b695
zhangsongshun <songshun3552@sina.com>
2018-04-11 15:44:18 +0800

    zhang

--- test8.py
+++ test8.py
@@ -2,9 +2,19 @@ from urllib.request import urlretrieve
 
 import requests
 # html = requests.get('http://pc.33egg.com:9101/?u=31899&k=xx')
+# print(html.text)
 from bs4 import BeautifulSoup
 
-html = requests.get('http://pc.33egg.com:9101/play.php?id=141')
+
+def back(a, b, c):
+    per = 100*a*b/c
+    if per > 100:
+        per = 100
+        print('下载完成！')
+    print('%.2f%%' % per)
+
+
+html = requests.get('http://pc.33egg.com:9101/play.php?id=57')
 soup = BeautifulSoup(html.text, 'html.parser')
 url = soup.select('.playbox-l')[0].select('.playContent source')[0]['src']
-urlretrieve(url, 'xxx.mp4')
+urlretrieve(url, 'xxx.mp4', back)
